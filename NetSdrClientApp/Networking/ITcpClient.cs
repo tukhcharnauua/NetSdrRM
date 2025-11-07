db4bcbd5bf0eaef64ef29c989c6040bce8dcf4b7
tukhcharnauua <127216430+tukhcharnauua@users.noreply.github.com>
2025-11-07 15:54:52 +0200

    Update ITcpClient.cs

--- NetSdrClientApp/Networking/ITcpClient.cs
+++ NetSdrClientApp/Networking/ITcpClient.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
-
 namespace NetSdrClientApp.Networking
 {
     public interface ITcpClient
